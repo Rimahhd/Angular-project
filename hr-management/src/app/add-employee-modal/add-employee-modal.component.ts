b1255a2926579913188172f3f0754b9d513f8d4d
Rimah Houssameldine <rimahhd3@gamil.com>
2024-05-29 20:01:24 +0300

    added the components

---
+++ hr-management/src/app/add-employee-modal/add-employee-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-employee-modal.component.html',
+  styleUrl: './add-employee-modal.component.css'
+})
+export class AddEmployeeModalComponent {
+
+}
---
+++ hr-management/src/app/add-leave-modal/add-leave-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-leave-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-leave-modal.component.html',
+  styleUrl: './add-leave-modal.component.css'
+})
+export class AddLeaveModalComponent {
+
+}
